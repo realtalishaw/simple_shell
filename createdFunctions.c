643d5f3dfca0cc69d8acf474996d8d784ee2a4bf
Lonzo Rust <rustlonzo@gmail.com>
2020-11-22 20:31:27 +0000

    added _getline function

--- createdFunctions.c
+++ createdFunctions.c
@@ -133,3 +133,26 @@ char *_strdup(char *source, unsigned int extra)
 
 	return (copy);
 }
+
+/**
+ * _getline - puts input from user into buffer line
+ * @inputBuffer: buffer for user input
+ * Return: buffer with user input
+ */
+char *_getline(FILE *inputBuffer)
+{
+	char *line;
+	ssize_t read;
+	size_t len;
+
+	line = NULL;
+	len = 0;
+	read = getline(&line, &len, inputBuffer);
+	if (read == -1)
+	{
+		free(line);
+		exit(EXIT_SUCCESS);
+	}
+
+	return (line);
+}

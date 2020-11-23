f5e7bbfbd5f602d8e9b7e54c94ddf2031795ba44
realtalishaw <misstalishawhite@gmail.com>
2020-11-23 16:19:03 -0600

    Update main.c

--- main.c
+++ main.c
@@ -28,4 +28,25 @@ int main(void) {
 }
 
 
+/**
+ * _strcat - gkjsgkjw;
+ * @dest: kfdjkla
+ * @src: fdjkkfjal
+ *
+ * Return: lkjdalk;f
+ */
 
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int len;
+
+	for (len = 0; dest[len] != 0;)
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+		dest[len + i] = src[i];
+	return (dest);
+}

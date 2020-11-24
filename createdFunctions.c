78a95252bd3aa9cc6c0375ce99d294c83c43c1a9
Lonzo Rust <60586779+lonzor@users.noreply.github.com>
2020-11-23 19:17:26 -0600

    Added _strcat function

--- createdFunctions.c
+++ createdFunctions.c
@@ -133,3 +133,26 @@ char *_strdup(char *source, unsigned int extra)
 
 	return (copy);
 }
+
+/**
+ * _strcat - gkjsgkjw;
+ * @dest: kfdjkla
+ * @src: fdjkkfjal
+ *
+ * Return: lkjdalk;f
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int len;
+
+	for (len = 0; dest[len] != 0;)
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+		dest[len + i] = src[i];
+	return (dest);
+}

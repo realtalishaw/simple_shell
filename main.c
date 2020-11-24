bdf2039c8db448cfd8cee4d606da312bc694f69b
realtalishaw <misstalishawhite@gmail.com>
2020-11-23 21:04:09 -0600

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(void) {
   do {
     if (isatty(STDIN_FILENO) == 1)
     {
-_putchar('$');
+putchar('$');
 
     }
     c = getline(&line, &size, stdin);
@@ -35,18 +35,6 @@ exit (0);
 
   return (0);
 }
-#include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 

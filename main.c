0c8aeda2974c68eff7cfc7c3e0a17609dcb31fab
realtalishaw <misstalishawhite@gmail.com>
2020-11-23 21:19:44 -0600

    Update main.c

--- main.c
+++ main.c
@@ -50,3 +50,25 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
+/**
+ * _strcat - gkjsgkjw;
+ * @dest: kfdjkla
+ * @src: fdjkkfjal
+ *
+ * Return: lkjdalk;f
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int len;
+
+	for (len = 0; dest[len] != 0;)
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+		dest[len + i] = src[i];
+	return (dest);
+}

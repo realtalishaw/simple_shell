4786c28f8090ad583f5bdf690648aaf805492627
Lonzo Rust <60586779+lonzor@users.noreply.github.com>
2020-11-23 18:54:15 -0600

    Update main.c
    changing main.c file for new checker attempt

--- main.c
+++ main.c
@@ -1,52 +1,34 @@
 #include "shell.h"
 
 int main(void) {
-  char *line = NULL;
+  char *line;
   char **args;
+  /*int i;*/
   size_t size = 0;
-  int status;
+  
+ line = NULL;
 
   do {
     putchar('$');
     putchar(' ');
+   
     getline(&line, &size, stdin);
     if (line[0] == '\n')
     {
       continue;
     }
-    /*printf("got line!\n");*/
-    args = tokenizer(line);
-    /*printf("got args!\n");*/
-    status = shell_builtin(args, line);
+    args = tokenizer(line);    
 
-    free(line);
-    free(args);
+    shell_builtin(args, line);
+      if (line != NULL)
+      {
+        free(line);
+        line = NULL;
+      }
+      /*free(args);*/
+      args = NULL;
   } while (1);
 
 
   return (0);
-}
-
-
-/**
- * _strcat - gkjsgkjw;
- * @dest: kfdjkla
- * @src: fdjkkfjal
- *
- * Return: lkjdalk;f
- */
-
-char *_strcat(char *dest, char *src)
-{
-	int i;
-	int len;
-
-	for (len = 0; dest[len] != 0;)
-	{
-		len++;
-	}
-
-	for (i = 0; src[i] != 0; i++)
-		dest[len + i] = src[i];
-	return (dest);
 }
